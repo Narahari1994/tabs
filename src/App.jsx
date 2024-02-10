05f5e986f41f0c543b9de19dc2f62d40b77f18af
Narahari1994 <narahariv94@gmail.com>
2024-02-10 12:49:58 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,25 @@
+import { useEffect } from "react";
+import { useState } from "react";
+
 const url = "https://course-api.com/react-tabs-project";
 
 const App = () => {
+  const [tabs, setTabs] = useState([]);
+
+  const fetchTabs = async () => {
+    try {
+      const res = await fetch(url);
+      const json = await res.json();
+      setTabs(json);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchTabs();
+  });
+
   return <div>Tabs Starter</div>;
 };
 export default App;
